e664a14e665ed2527c383d658907e15f04ccfd3b
bllack33 <bllack33@gmail.com>
2018-11-19 23:11:52 -0500

    productos

--- website/productos.php
+++ website/productos.php
@@ -12,8 +12,8 @@ $consultar = new Registro();
 $productos = $consultar->consultarProductos();
 
 $_SESSION['productos'] = $productos;
-print_r($_SESSION['productos']);
-//print_r(isset($_SESSION['productos']));
+
+
 ?>
 <style type="text/css">
 	.producto{  
@@ -21,16 +21,20 @@ print_r($_SESSION['productos']);
     margin-right: 145px;
     margin-top: 20px;
 	}
+	.numero{
+	    width: 60px;	
+	}
 </style>
 <div class="producto">
 	<table class="table">
-	  <thead>
+	  <thead class="thead-dark"> 
 	    <tr>
 	      <th scope="col">#</th>
 	      <th scope="col">Nombre</th>
 	      <th scope="col">Descripcion</th>
-	      <th scope="col">precio</th>
-	      <th scope="col">cantidad</th>
+	      <th scope="col">Precio</th>
+	      <th scope="col">Disponible</th>
+	      <th scope="col">Cantidad</th>
 	    </tr>
 	  </thead>
 	  <?php foreach ($productos as $row): ?>
@@ -41,6 +45,8 @@ print_r($_SESSION['productos']);
 	      <td><?php echo $row['descripcion'] ?></td>
 	      <td><?php echo $row['precio'] ?></td>
 	      <td><?php echo $row['cantidad'] ?></td>
+	      <td><input class="numero" type="text" placeholder="<?php echo '1'.'-'.$row['cantidad'] ?>"></td>
+	      <td><button class="text-warning" onclick="myFunction()" href="agregarCarrito.php?id=<?php echo $row['id']; ?>">Agregar</button></td>
 	    </tr>
 	  <?php endforeach ?>	    
 	  </tbody>
