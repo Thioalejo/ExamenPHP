a83a860a9a4124c7b994692533ad4ee4594714f6
bllack33 <bllack33@gmail.com>
2018-11-20 22:41:55 -0500

    full

--- website/comprar.php
+++ website/comprar.php
@@ -3,7 +3,8 @@ require ('includes/clases.php');
 $user = $_GET['user'];
 $eliminarcarrito = new Registro();
 $eliminarcarrito->pagarCarrito($user);
-
+unset($_SESSION['carrito']);
+unset($_SESSION['carrito']["total"]);
 header('Location: productos.php');
 
  ?>
